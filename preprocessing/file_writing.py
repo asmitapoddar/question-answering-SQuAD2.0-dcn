3db21f5f6befea2e651c71cfd9036558abd7e825
elisechaumat97 <47248467+elisechaumat97@users.noreply.github.com>
2020-03-12 21:41:21 +0000

    adding the span to the file

--- preprocessing/file_writing.py
+++ preprocessing/file_writing.py
@@ -13,6 +13,33 @@ def load_tokenize_dev_set():
         dev_set = json.load(f)
     return dev_set
 
+def get_token_index(char_index, tokens):
+    """
+    Given a char index, returns corresponding token locations.
+    If we're unable to complete the mapping e.g. because of special characters, we return None.
+        e.g. if context = "hello world" and context_tokens = ["hello", "world"] then
+        for 0,1,2,3,4 we return 0 and 6,7,8,9,10 we return 1.
+    """
+    acc = 0 # accumulator
+    token_index=None
+    current_token_index = 0 # current token location
+
+    for current_token in tokens:
+      for current_char in current_token:
+        if acc==char_index:
+          token_index=current_token_index
+        acc+=1
+      current_token_index+=1
+
+    return token_index
+
+def get_char_length(tokens):
+  len=0
+  for token in tokens:
+    for char in token:
+      len+=1
+  return len
+
 def preprocess(dataset, type):
     # Takes the parsed JSON dataset, and replaces the questions and context documents 
     # by a sequence of word embeddings after tokenisation.
@@ -21,6 +48,7 @@ def preprocess(dataset, type):
     data = dataset["data"]
     print("Computing embeddings for the text in SQuAD")
     examples=[]
+#    first=True
     for item in tqdm(data): 
         for para in item["paragraphs"]:
             
@@ -41,21 +69,45 @@ def preprocess(dataset, type):
                 # Each question typically has multiple answers
                 # Could be empty (if is_impossible = True, new in SQuAD v2)
                 for ans in qas["answers"]:
-                    
                     answer_text_tokens = ans["text"]
                     answer_start = ans["answer_start"]
-                    examples.append((' '.join(context_tokens), ' '.join(question_tokens), ' '.join(answer_text_tokens)))
+                    answer_end=answer_start+get_char_length(answer_text_tokens) ####get the right len
+                    assert answer_start <= answer_end
+                    answer_token_start_location=get_token_index(answer_start, context_tokens)
+                    answer_token_end_location=get_token_index(answer_end, context_tokens)
+                   # if type=='developing' and first:
+                    
+                    #  if answer_token_start_location==None:
+                     #   first=False
+                      #  print("answer_token_start_location=None")
+                      #  print(answer_start)
+                      #  print("context length")
+                      #  print(get_char_length(context_tokens))
+                      #  print(answer_text_tokens)
+                    
 
+                    #if answer_token_end_location==None:
+                     # print("answer_token_end_location=None")
+                      #print(answer_end)
+                    examples.append((' '.join(context_tokens), \
+                     ' '.join(question_tokens), \
+                     ' '.join(answer_text_tokens),\
+                     ' '.join([str(answer_token_start_location), str(answer_token_end_location)])))
+               
                     #print(answer_start)
     with open('data/preprocessed_'+type+'_context.txt', 'wb+') as context_file,  \
          open('data/preprocessed_'+type+'_question.txt', 'wb+') as question_file,\
-         open('data/preprocessed_'+type+'_ans_text.txt', 'wb+') as ans_text_file :
+         open('data/preprocessed_'+type+'_ans_text.txt', 'wb+') as ans_text_file,\
+         open('data/preprocessed_'+type+'_ans_span.txt', 'wb+') as ans_span_file :
+
     
     # Write to files 
-      for (context, question, answer) in examples:
+      for (context, question, answer, span) in examples:
           context_file.write(context.encode('utf8') + b'\n')
           question_file.write(question.encode('utf8') + b'\n')
           ans_text_file.write(answer.encode('utf8') + b'\n')
+          ans_span_file.write(span.encode('utf8') + b'\n')
+
 
 
 def main():
