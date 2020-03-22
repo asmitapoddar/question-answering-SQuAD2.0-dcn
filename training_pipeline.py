77b245463ab2670c3842dee4dd924bed86981969
Asmita Poddar <asmita.poddar@gtc.ox.ac.uk>
2020-03-22 01:31:50 +0000

    Update training_pipeline.py

--- training_pipeline.py
+++ training_pipeline.py
@@ -73,6 +73,23 @@ def get_data(self, batch, is_train=True):
             return qn_seq_var, qn_mask_var, context_seq_var, context_mask_var, span_var
         else:
             return qn_seq_var, qn_mask_var, context_seq_var, context_mask_var
+   
+def get_grad_norm(self, parameters, norm_type=2):
+        parameters = list(filter(lambda p: p.grad is not None, parameters))
+        total_norm = 0
+        for p in parameters:
+            param_norm = p.grad.data.norm(norm_type)
+            total_norm += param_norm ** norm_type
+        total_norm = total_norm ** (1. / norm_type)
+        return total_norm
+
+def get_param_norm(self, parameters, norm_type=2):
+        total_norm = 0
+        for p in parameters:
+            param_norm = p.data.norm(norm_type)
+            total_norm += param_norm ** norm_type
+        total_norm = total_norm ** (1. / norm_type)
+        return total_norm
 
 
 def train_one_batch(self, batch, model, optimizer, params):
