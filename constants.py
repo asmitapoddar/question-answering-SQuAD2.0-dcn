51306ab78fbf23375652c1b228bcf81e64f79a3c
Asmita Poddar <asmita.poddar@gtc.ox.ac.uk>
2020-03-22 01:41:44 +0000

    Add more constants

--- constants.py
+++ constants.py
@@ -1,8 +1,18 @@
 # Defaults/constants.
+
 BATCH_SIZE = 64
 DROPOUT = 0.3
 HIDDEN_DIM = 200  # Denoted by 'l' in the paper.
 MAX_ITER = 4
 MAXOUT_POOL_SIZE = 16
 
+context_len = 600
+question_len = 30
+
 TEST_DCN_MODEL_WITH_CPU = False
+
+_PAD = b"<pad>"
+_UNK = b"<unk>"
+_START_VOCAB = [_PAD, _UNK]
+PAD_ID = 0
+UNK_ID = 1
