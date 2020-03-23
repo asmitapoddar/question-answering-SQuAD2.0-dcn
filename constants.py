c929606ab4fb136b1f1e45a667579872da632d4a
Asmita Poddar <asmita.poddar@gtc.ox.ac.uk>
2020-03-23 19:58:51 +0000

    Update constants.py

--- constants.py
+++ constants.py
@@ -5,12 +5,10 @@ DROPOUT = 0.3
 HIDDEN_DIM = 200  # Denoted by 'l' in the paper.
 MAX_ITER = 4
 MAXOUT_POOL_SIZE = 16
-
 context_len = 600
 question_len = 30
-
+reg_lambda - 0.1
 TEST_DCN_MODEL_WITH_CPU = False
-
 _PAD = b"<pad>"
 _UNK = b"<unk>"
 _START_VOCAB = [_PAD, _UNK]
