f4348bd7d61c4d08285cb1c221b2cd1aa5830580
jperlin <jakubperlin@gmail.com>
2020-03-25 21:41:42 +0000

    Try to fix embedding_matrix.py -- please check
    I restored Asmita's latest version and then added the 'go through glove vecs' part from Richie.

--- preprocessing/embedding_matrix.py
+++ preprocessing/embedding_matrix.py
@@ -1,3 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.autograd import Variable
+import numpy as np
+import csv
+import json
+from google.colab import files
 
 """This file contains a function to read the GloVe vectors from file,
 and return them as an embedding matrix"""
@@ -21,6 +30,7 @@ def get_glove(glove_path, glove_dim):
     vocab_size = 2196018 # this is the vocab size of the corpus we've downloaded
 
     emb_matrix = np.zeros((vocab_size + len(_START_VOCAB), glove_dim))
+    print(emb_matrix.shape)
     word2id = {}
     id2word = {}
 
@@ -32,17 +42,39 @@ def get_glove(glove_path, glove_dim):
     # put start tokens in the dictionaries
     idx = 0
     for word in _START_VOCAB:
+        print('word', idx)
         word2id[word] = idx
         id2word[idx] = word
         idx += 1
-
+        
     # go through glove vecs
     with open(glove_path, 'r') as fh:
         for line in tqdm(fh):
             values = line.split()
             word = ''.join(line[:-(len(' '.join(values[-300:]))+2)])
+
+            vector = list(map(float, values[-300:]))
+
+    # Length of vocab
+    """
+    with open(glove_path, 'r') as f:
+        for i, l in enumerate(f):
+            pass
+        print('Length',i+1)
+    """
+    print('index', idx)
+    #k=1
+    # go through glove vecs
+    with open(glove_path, 'r') as fh:
+        for line in tqdm(fh, total=20):
+            #print(k)
+            #k=k+1
+            values = line.split()
+            word = ''.join(line[:-(len(' '.join(values[-300:]))+2)])
+            #print(line, word)
             
             vector = list(map(float, values[-300:]))
+            #print('Length', len(vector))
             if glove_dim != len(vector):
                 continue;
                 raise Exception("You set --embedding_size=%i. If you set --glove_path yourself then make sure that --embedding_size=%i matches!" % (glove_dim, len(vector)))
@@ -58,3 +90,18 @@ def get_glove(glove_path, glove_dim):
     #assert idx == final_vocab_size
 
     return emb_matrix, word2id, id2word
+
+#Write to file
+"""
+#For dictionary
+w = csv.writer(open("word2id.csv", "w"))
+for key, val in word2id.items():
+    w.writerow([key, val])
+files.download('word2id.csv') #for downloading to Google Drive
+
+#For embedding_matrix
+np.savetxt('embedding_matrix.txt', emb_matrix,fmt='%.2f')
+
+f=open("embedding_matrix.txt", "r")
+embedding_matrix = f.read()
+"""
