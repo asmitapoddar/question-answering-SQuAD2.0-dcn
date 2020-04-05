553099c109400d51e7f676309338d94078e94760
Richie SSH <richie.yeung1@gmail.com>
2020-04-05 17:09:41 +0800

    update plotter

--- gen_plot.py
+++ gen_plot.py
@@ -1,18 +1,24 @@
 import time
-import subprocess
 import select
+import subprocess
+import sys
 import math
 
 import matplotlib.pyplot as plt
-with open("loss.log", "r") as f:
-    data = list(map(lambda s: tuple(s[:-1].split(": ")), f.readlines()))
+
+loss_path = "loss.log" if len(sys.argv) <= 1 else sys.argv[1]
+
+history = 200
+
+with open(loss_path, "r") as f:
+    data = list(map(lambda s: tuple(s[:-1].split(": ")), f.readlines()[-history:]))
     x = list(map(lambda d: int(d[0]), data))
-    y = list(map(lambda d: math.log10(float(d[1])), data))
+    y = list(map(lambda d: float(d[1]), data))
     print(x, y)
 plt.plot(x, y, 'm-')
 plt.pause(1)
 
-filename = "loss.log"
+filename = loss_path
 f = subprocess.Popen(['tail','-F','-n 0',filename],\
         stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 p = select.poll()
@@ -23,7 +29,7 @@ while True:
         line = f.stdout.readline()[:-1].decode("utf-8").split(": ")
         print(line)
         x += [int(line[0])]
-        y += [math.log10(float(line[1]))]
+        y += [float(line[1])]
         plt.plot(x, y, 'm-')
         plt.pause(0.1)
     time.sleep(1)
