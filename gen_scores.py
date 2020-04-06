1d5953b189148267de0918786b010a641dd7c4dd
jperlin <jakubperlin@gmail.com>
2020-04-06 19:35:08 +0100

    Improve console output of gen_scores

--- gen_scores.py
+++ gen_scores.py
@@ -7,16 +7,20 @@ import sys
 import torch as th
     
 DEFAULT_FREQ = 200  # Evals every 200 global steps.
-TEMP_JSON_FILENAME = "kuba_temp.json"
+TEMP_JSON_FILENAME = "gen_scores_temp.json" # Doesn't matter, just didn't want it to clash with other people's temp files.
+
+def custom_print(x, flush=False):
+    print("[gen_scores] ",end='',flush=flush)
+    print(x)
 
 def gen_predictions(model_path, dataset_path, glove):
     tokenized_dataset_path = dataset_path.split(".")[0]+"-tokenized.json"
-    print("Calling produce_answers.run_evaluation()...")
+    custom_print("Calling produce_answers.run_evaluation()...")
     run_evaluation(str(model_path), tokenized_dataset_path, TEMP_JSON_FILENAME, glove)
 
 def run_eval(dataset_path):
     cmd = ["python3", "evaluate-v2.0.py", dataset_path, TEMP_JSON_FILENAME]
-    print("Calling subprocess '%s'..." % " ".join(list(map(str,cmd))))
+    custom_print("Calling subprocess '%s'..." % " ".join(list(map(str,cmd))))
     p = subprocess.run(["python3", "evaluate-v2.0.py", dataset_path, TEMP_JSON_FILENAME], check=True, stdout=subprocess.PIPE, universal_newlines=True)
     outp = p.stdout
     em_score = outp.split('"exact": ')[1].split(",")[0]
@@ -26,8 +30,8 @@ def run_eval(dataset_path):
 
 def main():
     if len(sys.argv) not in [3,4]:
-        print("Usage example: \npython3 gen_scores.py ./model/2020-04-07_00-10-37\[LR1-00e-03_Q86821_B64_H200_RS1\]/ preprocessing/data/subset-4/train-subset-4.json")
-        print("Can add optional FREQ parameter at the end (integer regulating frequency of evaluation measured in global steps).")
+        custom_print("Usage example: \npython3 gen_scores.py ./model/2020-04-07_00-10-37\[LR1-00e-03_Q86821_B64_H200_RS1\]/ preprocessing/data/subset-4/train-subset-4.json")
+        custom_print("Can add optional FREQ parameter at the end (integer regulating frequency of evaluation measured in global steps).")
         return
 
     model_dir = sys.argv[1]
@@ -40,7 +44,7 @@ def main():
 
     next_global_step_to_eval = 0
     if os.path.exists(model_dir+"scores.log"):
-        print("scores.log already exists. Do you want to overwrite it?[y/n]")
+        custom_print("scores.log already exists. Do you want to overwrite it?[y/n]")
         user_ans = str(input())
         if "y" not in user_ans:
             return
@@ -52,7 +56,7 @@ def main():
             global_step = th.load(model_path)[SERIALISATION_KEY_GLOBAL_STEP]
             if global_step >= next_global_step_to_eval:
                 next_global_step_to_eval += freq
-                print("Evaluating model: '%s' ..." % model_path, flush=True)
+                custom_print("Evaluating model: '%s' ..." % model_path,flush=True)
                 gen_predictions(model_path, dataset_path, glove)
                 em_score, f1_score, total = run_eval(dataset_path)
                 scores_file.write(str(global_step) + "," + str(em_score) + "," + str(f1_score) + "," + str(total) + "\n")
