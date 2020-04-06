19b39e6e23a1bd27b59e6621340b30d034d54cdd
jperlin <jakubperlin@gmail.com>
2020-04-06 18:14:34 +0100

    Bugfixes gen_scores.py

--- gen_scores.py
+++ gen_scores.py
@@ -11,12 +11,12 @@ TEMP_JSON_FILENAME = "kuba_temp.json"
 def gen_predictions(model_path, dataset_path):
     tokenized_dataset_path = dataset_path.split(".")[0]+"-tokenized.json"
     cmd = ["python3", "produce_answers.py", model_path, tokenized_dataset_path, TEMP_JSON_FILENAME]
-    print("Calling subprocess '%s'..." % " ".join(cmd))
+    print("Calling subprocess '%s'..." % " ".join(list(map(str,cmd))))
     subprocess.run(cmd)
 
 def run_eval(dataset_path):
     cmd = ["python3", "evaluate-v2.0.py", dataset_path, TEMP_JSON_FILENAME]
-    print("Calling subprocess '%s'..." % " ".join(cmd))
+    print("Calling subprocess '%s'..." % " ".join(list(map(str,cmd))))
     p = subprocess.run(["python3", "evaluate-v2.0.py", dataset_path, TEMP_JSON_FILENAME], check=True, stdout=subprocess.PIPE, universal_newlines=True)
     outp = p.stdout
     em_score = outp.split('"exact": ')[1].split(",")[0]
