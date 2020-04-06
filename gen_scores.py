2619357524f13474672a8df196e884f54e1d147d
jperlin <jakubperlin@gmail.com>
2020-04-06 19:29:51 +0100

    Bugfixes gen_scores.py

--- gen_scores.py
+++ gen_scores.py
@@ -24,33 +24,38 @@ def run_eval(dataset_path):
     total = outp.split('"total": ')[1].split(",")[0]
     return em_score, f1_score, total
 
-if __name__ == "__main__":
+def main():
     if len(sys.argv) not in [3,4]:
         print("Usage example: \npython3 gen_scores.py ./model/2020-04-07_00-10-37\[LR1-00e-03_Q86821_B64_H200_RS1\]/ preprocessing/data/subset-4/train-subset-4.json")
         print("Can add optional FREQ parameter at the end (integer regulating frequency of evaluation measured in global steps).")
-    else:
-        glove = load_embeddings_index() # Do this once.
+        return
 
-        model_dir = sys.argv[1]
-        if model_dir[-1] != '/':
-            model_dir += '/'
-        dataset_path = sys.argv[2]
-        freq = DEFAULT_FREQ if len(sys.argv)==3 else sys.argv[3]
+    model_dir = sys.argv[1]
+    if model_dir[-1] != '/':
+        model_dir += '/'
+    dataset_path = sys.argv[2]
+    freq = DEFAULT_FREQ if len(sys.argv)==3 else sys.argv[3]
 
-        next_global_step_to_eval = 0
-        if os.path.exists(model_dir+"scores.log"):
-            print("scores.log already exists. Do you want to overwrite it?[y/n]")
-            user_ans = str(input())
-            if "y" in user_ans:
-                with open(model_dir+"scores.log", "w") as scores_file:
-                    model_paths = sorted(Path(model_dir).iterdir(), key=os.path.getmtime)
-                    model_paths = [p for p in model_paths if ".par" in str(p)]
-                    for i, model_path in enumerate(model_paths):
-                        assert(".par" in str(model_path))
-                        global_step = th.load(model_path)[SERIALISATION_KEY_GLOBAL_STEP]
-                        if global_step >= next_global_step_to_eval:
-                            next_global_step_to_eval += freq
-                            print("Evaluating model: '%s' ..." % model_path, flush=True)
-                            gen_predictions(model_path, dataset_path, glove)
-                            em_score, f1_score, total = run_eval(dataset_path)
-                            scores_file.write(str(global_step) + "," + str(em_score) + "," + str(f1_score) + "," + str(total) + "\n")
+    glove = load_embeddings_index() # Do this once.
+
+    next_global_step_to_eval = 0
+    if os.path.exists(model_dir+"scores.log"):
+        print("scores.log already exists. Do you want to overwrite it?[y/n]")
+        user_ans = str(input())
+        if "y" not in user_ans:
+            return
+    with open(model_dir+"scores.log", "w") as scores_file:
+        model_paths = sorted(Path(model_dir).iterdir(), key=os.path.getmtime)
+        model_paths = [p for p in model_paths if ".par" in str(p)]
+        for i, model_path in enumerate(model_paths):
+            assert(".par" in str(model_path))
+            global_step = th.load(model_path)[SERIALISATION_KEY_GLOBAL_STEP]
+            if global_step >= next_global_step_to_eval:
+                next_global_step_to_eval += freq
+                print("Evaluating model: '%s' ..." % model_path, flush=True)
+                gen_predictions(model_path, dataset_path, glove)
+                em_score, f1_score, total = run_eval(dataset_path)
+                scores_file.write(str(global_step) + "," + str(em_score) + "," + str(f1_score) + "," + str(total) + "\n")
+
+if __name__ == "__main__":
+    main()
