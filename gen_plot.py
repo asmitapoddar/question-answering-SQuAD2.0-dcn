9bb746e6b998921b465f6ff54f0fc10a933b2dc9
jperlin <jakubperlin@gmail.com>
2020-04-07 18:36:10 +0100

    Implement running-avg smoothing for gen_plot.py (loss plots)

--- gen_plot.py
+++ gen_plot.py
@@ -8,13 +8,27 @@ import matplotlib.pyplot as plt
 
 loss_path = "loss.log" if len(sys.argv) <= 1 else sys.argv[1]
 
-history = 200
+HISTORY = 200
+SMOOTHING = 0  # Ballpark values: try setting to 5 for some smoothing or 20 for a lot of smoothing.
 
 with open(loss_path, "r") as f:
-    data = list(map(lambda s: tuple(s[:-1].split(": ")), f.readlines()[-history:]))
+    data = list(map(lambda s: tuple(s[:-1].split(": ")), f.readlines()[-HISTORY:]))
+    data = sorted(list(filter(lambda tup: len(tup)==2, data)))
     x = list(map(lambda d: int(d[0]), data))
     y = list(map(lambda d: float(d[1]), data))
-    print(x, y)
+
+    if SMOOTHING > 0:
+        print("Using smoothing by running average of width %d." % (2*SMOOTHING+1))
+        for i in range(len(y)):
+            ynew = 0.0
+            count = 0
+            for k in range(i-SMOOTHING,i+SMOOTHING+1):
+                if 0 <= k < len(y):
+                    ynew += y[k]
+                    count += 1
+            y[i] = ynew/float(count)
+
+    #print(x, y)
 plt.plot(x, y, 'm-')
 plt.pause(1)
 
